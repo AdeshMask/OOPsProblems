bb165f36a386278046c463a9dd63613db025641b
AdeshMask <adeshmaske255@gmail.com>
2022-03-23 11:33:07 +0530

    Added: Employee Daily Wages

--- src/com/bridgelabz/Day9/EmployeeWage.java
+++ src/com/bridgelabz/Day9/EmployeeWage.java
@@ -1,14 +1,23 @@
 package com.bridgelabz.Day9;
 
 public class EmployeeWage {
+
+    static final int WAGE_PER_HOUR = 20;
+    static final int FULL_DAY_HOURS = 8;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Problem::");
+        int dailyWages = 0;
         int attendance = (int) Math.floor(Math.random()*10)%3;
         if (attendance == 0)
             System.out.println("Emloyee is absent");
-        else if (attendance == 1)
+        else if (attendance == 1) {
             System.out.println("Employee is present");
-        else
+            dailyWages = WAGE_PER_HOUR * FULL_DAY_HOURS;
+            System.out.println("Daily Wages of Employee: "+dailyWages);
+        }
+        else {
             System.out.println("Employee is present for the Half day");
+        }
     }
 }
